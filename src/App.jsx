2fe3830a0f149ae4c1041dca895786c996f7bc9a
Ana Caroline <caroline.ana.n.s@gmail.com>
2024-06-06 10:00:20 -0500

    feat:  cria um card e aplica style

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ function App() {
 
   return (
     <>
-      <h1> App</h1>
+     <div className='card'>
+      <h2>Rick Sanchez</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+     </div>
     </>
   )
 }
 
 export default App
+ 
